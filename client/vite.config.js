f7dca563654894fad91dcc8d5aba05194fbae9f5
anjdulam <anjalidulam03@gmail.com>
2024-04-30 21:41:32 +0530

    first

---
+++ client/vite.config.js
@@ -0,0 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 4004
+//   }
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    // Set the entry file for your front end here
+    rollupOptions: {
+      input:'client/src/Main.jsx'
+    }
+  }
+})
